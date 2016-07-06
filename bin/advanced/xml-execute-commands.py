91765755dc7953a95c10bb7a76cf8ec06444885b
Matt Harasymczuk <matt@harasymczuk.pl>
2016-07-06 12:00:48 +0200

    Introspection and logging

--- bin/advanced/xml-execute-commands.py
+++ bin/advanced/xml-execute-commands.py
@@ -5,31 +5,34 @@ import xml.etree.ElementTree
 import subprocess
 
 FILENAME = 'xml-execute-commands.xml'
+LOG_FORMAT = '[%(levelname)-5s] %(filename)s:%(lineno)s - %(msg).110s'
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 log = logging.getLogger('code-execution')
 root = xml.etree.ElementTree.parse(FILENAME).getroot()
 
 
 def run(command, timeout=1):
     log.info('Executing command: %s' % command)
+
     with subprocess.Popen(command, stdout=subprocess.PIPE) as proc:
+
         try:
             output, errors = proc.communicate(timeout=timeout)
-
-            if errors:
-                log.error(errors)
-
-            if output:
-                log.warning(output)
-                return output
-
         except subprocess.TimeoutExpired:
-            log.critical('Timeout %s exceeded for command: %s' % (timeout, command))
-            proc.kill()
+            log.error('Timeout %s exceeded for command: %s' % (timeout, command))
+            return proc.kill()
+
+        if errors:
+            log.error(errors)
+
+        if output:
+            log.debug(output)
+            return output
 
 
 for command in root.findall('./command'):
     cmd = command.text.split()
-    timeout = float(command.get('timeout', 0))
+    timeout = float(command.get('timeout', 1))
     run(cmd, timeout)
