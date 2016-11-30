91167c8b737d0a6dbe5d0be385ac3447d0a4f998
Matt Harasymczuk <matt@harasymczuk.pl>
2016-11-30 12:02:15 +0100

    Fixtures with habitat data

--- bin/2016-11-29/przyklad5-json.py
+++ bin/2016-11-29/przyklad5-json.py
@@ -1,9 +1,9 @@
 from pprint import pprint
 import json
-from datetime import datetime
-from datetime import timezone
+import datetime
 
-FILENAME = '../tmp/fixtures.json'
+INPUT = '../tmp/fixtures.json'
+OUTPUT = '../tmp/fixtures-new.json'
 
 
 def make_datetime(string):
@@ -11,12 +11,48 @@ def make_datetime(string):
     >>> make_datetime('2013-10-21T13:28:06.419Z')
     datetime.datetime(2013, 10, 21, 13, 28, 6, 419000, tzinfo=datetime.timezone.utc)
     """
-    return datetime.strptime(string, '%Y-%m-%dT%H:%M:%S.%fZ').replace(
-        tzinfo=timezone.utc)
+    return datetime.datetime.strptime(string, '%Y-%m-%dT%H:%M:%S.%fZ').replace(
+        tzinfo=datetime.timezone.utc)
 
 
-with open(FILENAME) as file:
+with open(INPUT) as file:
     content = file.read()
 
-for key, value in json.loads(content).items():
-    print(value)
+
+data = json.loads(content)
+
+for key, value in data.items():
+    for element in value:
+        element['timestamp'] = make_datetime(element['timestamp'])
+
+"""
+class DatetimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        try:
+            return super(DatetimeEncoder, obj).default(obj)
+        except TypeError:
+            return '{:%Y-%m-%dT%H:%M:%S.%fZ}'.format(obj)
+
+
+with open(OUTPUT, 'w') as file:
+    d = json.dumps(data, cls=DatetimeEncoder)
+    file.write(d)
+"""
+
+#d = json.dumps(data, indent=4, sort_keys=True, default=lambda x:str(x))
+#print(d)
+
+json.JSONEncoder.default = lambda self,obj: ('{:%Y-%m-%dT%H:%M:%S.%fZ}'.format(obj) if isinstance(obj, datetime.datetime) else None)
+
+
+def _(obj):
+    if isinstance(obj, datetime.datetime):
+        # return '{:%Y-%m-%dT%H:%M:%S.%fZ}'.format(obj)
+        return obj.isoformat()
+    else:
+        return None
+
+
+
+d = json.dumps(data)
+print(d)
--- podstawy/serializacja.rst
+++ podstawy/serializacja.rst
@@ -7,8 +7,25 @@ Pickle
 ``json``
 --------
 
+.. code:: python
+
+    import json
+    json.loads()
+    json.dumps()
+
 ``csv``
 -------
+
+.. code:: python
+
+    import csv
+
+    FIELDNAMES = ['id', 'username', 'address']
+
+
+    with open(FILENAME) as file:
+        for row in csv.DictReader(file, delimiter=' ', quotechar='"', fieldnames=FIELDNAMES):
+            output.write(TEMPLATE.format(**clean(row)))
 
 
 xml
